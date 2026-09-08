56e67466c997334637dd34f7b309eb67070c0b24
lmart302 <lmart302@gmail.com>
2026-09-08 21:09:05 +0900

    네온효과보강2

--- weight.js
+++ weight.js
@@ -522,7 +522,7 @@ function playWeightNeon(activeType = "monthly") {
 
   setTimeout(() => {
     section.classList.remove("neon-active");
-  }, 3000);
+  }, 2000);
 }
 
 function switchView(activeType) {
