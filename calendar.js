d9cf7523dfa1a6a4a503400e8f96197cc13872c3
lmart302 <lmart302@gmail.com>
2026-09-21 20:29:30 +0900

    걸음을보로

--- calendar.js
+++ calendar.js
@@ -101,7 +101,7 @@ function drawCalendar() {
 if (record.running) {
     exerciseText = `🏃 ${record.running}분`;
 } else if (record.steps) {
-    exerciseText = `🚶 ${Number(record.steps).toLocaleString()}걸음`;
+    exerciseText = `🚶 ${Number(record.steps).toLocaleString()}보`;
 }
 
 html += `<div class='record-line'>${exerciseText}</div>`;
